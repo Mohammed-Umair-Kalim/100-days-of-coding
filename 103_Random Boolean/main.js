27a12f8b8358893d659144aca345b68c95087762
Mohammed-Umair-Kalim <umairkalim@gmail.com>
2024-04-04 14:32:07 -0700

    Day 35th Challenge Completed
    I have completed Q.no 103,104 & 105 form 100Days of coding challenge of typescript

---
+++ 103_Random Boolean/main.js
@@ -0,0 +1,12 @@
+"use strict";
+// Question 103: Write a function that returns a random boolean value, true or false ?
+// Explain & TIP: Generating a random boolean is a simple but useful trick. You can use Math.random()
+// and check if it's above or below 0.5 to decide between true or false
+// Answer 103:
+let randomNumber = Math.random();
+function random_boolean(num = 0.5) {
+    let random = randomNumber > num;
+    return random;
+}
+console.log(random_boolean());
+console.log(Math.random());
