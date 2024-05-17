1baa4ada8cbbf1c43188f21e4638d4fae7b6766a
Mohammed-Umair-Kalim <umairkalim@gmail.com>
2024-05-18 00:15:29 +0500

    Updated Day 65 TypeScript Challenge
    I have completed Day 65 typescript challenge issued on discord in GIAIC (Governor Initiative of Artificial Intelligence & Computing) channel by Sir Asharib Ali

--- 165_Function & Annotation/dist/main.js
+++ 165_Function & Annotation/dist/main.js
@@ -1,21 +1,20 @@
 "use strict";
-// // Function:
-// function sum () {
-//     console.log("Result of Function:",5 + 2);
-// }
 Object.defineProperty(exports, "__esModule", { value: true });
-// sum() // Invoking function, Outputs 7
-// // Function with parameters and annotation:
-// function adding (a:number , b:number) {
-//     console.log("Result of Function with parameters & annotation:",a + b);
-// }
-// adding(5 , 5); // Invoking function, Outputs 10
-// // Default parameters:
-// function food (item1:string = "Pizza" , item2:string = "Ketchup" ) //Assigning default values to parameters
-// {
-//     console.log(`Bring ${item1} with ${item2}`)
-// }
-// food() //If no arguments is passed, it outputs default values stored in parameters
+// Function:
+function sum() {
+    console.log("Result of Function:", 5 + 2);
+}
+sum(); // Invoking function, Outputs 7
+// Function with parameters and annotation:
+function adding(a, b) {
+    console.log("Result of Function with parameters & annotation:", a + b);
+}
+adding(5, 5); // Invoking function, Outputs 10
+// Default parameters:
+function food(item1 = "Pizza", item2 = "Ketchup") {
+    console.log(`Bring ${item1} with ${item2}`);
+}
+food(); //If no arguments is passed, it outputs default values stored in parameters
 // Optional parameter:
 function person(name, age) {
     console.log(`A person's name is: ${name} and person's age is: ${age}`);
