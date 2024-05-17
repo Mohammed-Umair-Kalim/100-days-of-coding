ee25f8d05b684418f1e9e9792f5f87f9f8a92347
Mohammed-Umair-Kalim <umairkalim@gmail.com>
2024-05-17 15:41:10 +0500

    Day 63 Challenge Completed
    I have completed Day 63 typescript challenge issued on discord by GIAIC (Governor Initiative of Artificial Intelligence & Computing)

---
+++ 163_Multiple Types In TypeScript/src/main.ts
@@ -0,0 +1,12 @@
+// Mulitple types in TypeScript:
+
+//Dynamic Type:
+let text = "Hi there";
+console.log(text)
+
+//Static type:
+let text2:string = "Good Morning!"; // Annotating a variable with string type
+console.log(text2)
+
+let num:number = 123; // Annotating a variable with number type
+console.log(num);
