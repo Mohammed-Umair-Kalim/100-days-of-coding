57e5a0558648eed6381dd50b23ea85f9461fcb74
Mohammed-Umair-Kalim <umairkalim@gmail.com>
2024-05-18 14:20:08 +0500

    Day 169 TypeScript Completed
    I have completed Day 69 typescript challenge issued on discord in GIAIC (Governor Initiative of Artificial Intelligence & Computing) channel by Sir Asharib Ali

---
+++ 169_Enums/src/main.ts
@@ -0,0 +1,24 @@
+// Enums:
+enum Months 
+{
+    January,
+    February,
+    March,
+    April,
+    May,
+    June,
+    July,
+    August,
+    September,
+    October,
+    November,
+    December,
+}
+  
+console.log("\n");
+
+console.log(`Current month is: ${Months[Months.May]}`); //here using dot notation for the output
+
+console.log("\n");
+  
+console.log(`Current month is: ${Months[4]}`); //here using indexing for the output
