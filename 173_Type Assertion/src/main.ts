acbb44de1282455905d49e6a094247f91517037d
Mohammed-Umair-Kalim <umairkalim@gmail.com>
2024-05-25 17:13:28 +0500

    Day73 TypeScript Challenge Completed
    I have completed Day 73 typescript challenge issued on discord in GIAIC (Governor Initiative of Artificial Intelligence & Computing) channel by Sir Asharib Ali

---
+++ 173_Type Assertion/src/main.ts
@@ -0,0 +1,7 @@
+// Type Assertion:
+let input: any = "abc";
+
+// Type assertion set "input" type as a string
+console.log((input as string));
+
+console.log("after using type assertion:",typeof(input));
